811c3587911d70fe0c5cfb1ed07f7a5ccf8e805a
Antoine <antoinelevanen@gmail.com>
2024-10-27 11:32:34 +0100

    add random number generation

--- include/rtweekend.hpp
+++ include/rtweekend.hpp
@@ -3,6 +3,7 @@
 // Standard Headers
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -24,7 +25,20 @@ inline double deg2rad(double degrees)
     return degrees * pi / 180.0;
 }
 
+inline double randomDouble()
+{
+    // Return a random real in [0, 1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double randomDouble(double min, double max)
+{
+    // Return a random real in [min, max).
+    return min + (max - min) * randomDouble();
+}
+
 // Common Headers
+// They must be included here since they use  the infinity and py constante.
 
 #include "color.hpp"
 #include "ray.hpp"
