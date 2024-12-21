fb6a5ed6f325004f66fc48e17a259f29e8817ecb
Antoine <antoinelevanen@gmail.com>
2024-12-21 13:02:45 +0100

    add metal materials

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,32 @@
 #include "camera.hpp"
 #include "hittable.hpp"
 #include "hittable_list.hpp"
+#include "material.hpp"
 #include "sphere.hpp"
 
 int main() {
-
-    
 
     // World
 
     HittableList world;
 
-    world.add(make_shared<Sphere>(Point3(0, 0, -1), 0.5));
-    world.add(make_shared<Sphere>(Point3(0, -100.5, -1), 100));  
+    auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.001);
+    auto material_right = make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.75);
+
+    world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100, material_ground));  
+    world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     Camera camera;
     // Create a PPM image file
     std::ofstream render_image("../render/output.ppm");
 
-    camera.image_width = 400;
-    camera.image_height = 200;
-    camera.sample_per_pixel = 100;
+    camera.image_width = 1600;
+    camera.image_height = 900;
+    camera.sample_per_pixel = 50;
     camera.max_depth = 50;
 
     camera.render(render_image, world);
