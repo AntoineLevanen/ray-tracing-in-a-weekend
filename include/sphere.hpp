3994e6cde58036fe09cc79c12f62d6c176f6d91c
Antoine <antoinelevanen@gmail.com>
2025-01-11 15:55:22 +0100

    add moving sphere

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -5,14 +5,18 @@
 class Sphere : public Hittable
 {
   public:
-    Sphere(const Point3 &center, double radius, shared_ptr<Material> material) : center(center), radius(std::fmax(0, radius)) , material(material)
-    {
-      // TODO: Initialize the material pointer 'material' in record.
-    }
+    // Stationary Sphere
+    Sphere(const Point3 &static_center, double radius, shared_ptr<Material> material) 
+      : center(static_center, Vector3(0,0,0)), radius(std::fmax(0, radius)) , material(material) {}
+
+    // Stationary Sphere
+    Sphere(const Point3 &center1, const Point3 &center2, double radius, shared_ptr<Material> material) 
+      : center(center1, center2 - center1), radius(std::fmax(0, radius)) , material(material) {}
 
     bool hit(const Ray &ray, Interval ray_t, HitRecord &record) const override 
     {
-      Vector3 oc = center - ray.origin();
+      Point3 current_center = center.at(ray.time());
+      Vector3 oc = current_center - ray.origin();
       auto a = ray.direction().length_squared();
       auto h = dot(ray.direction(), oc);
       auto c = oc.length_squared() - radius*radius;
@@ -38,7 +42,7 @@ class Sphere : public Hittable
 
       record.t = root;
       record.hit_impact= ray.at(record.t);
-      Vector3 outward_normal = (record.hit_impact- center) / radius;
+      Vector3 outward_normal = (record.hit_impact- current_center) / radius;
       record.setFaceNormal(ray, outward_normal);
       record.material = material;
 
@@ -46,7 +50,7 @@ class Sphere : public Hittable
     }
   
   private:
-    Point3 center;
+    Ray center;
     double radius;
     shared_ptr<Material> material;
 };
