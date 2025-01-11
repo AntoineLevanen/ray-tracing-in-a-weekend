42ede4c99977ab60b0c624cfb40f1aa6bf236a10
Antoine <antoinelevanen@gmail.com>
2025-01-11 15:47:29 +0100

    add time to ray class

--- include/ray.hpp
+++ include/ray.hpp
@@ -5,19 +5,25 @@
 class Ray
 {
 public:
-    Ray() {}
+  Ray() {}
 
-    Ray(const Point3& origin, const Vector3& direction) : ray_origin(origin), ray_direction(direction) {}
+  Ray(const Point3& origin, const Vector3& direction) 
+    : ray_origin(origin), ray_direction(direction) {}
 
-    const Point3& origin() const { return ray_origin; }
-    const Vector3& direction() const { return ray_direction; }
+  Ray(const Point3& origin, const Vector3& direction, double time) 
+    : ray_origin(origin), ray_direction(direction), ray_time(time) {}
 
-    Point3 at(double t) const
-    {
-        return ray_origin + t * ray_direction;
-    }
+  const Point3& origin() const { return ray_origin; }
+  const Vector3& direction() const { return ray_direction; }
+  double time() const { return ray_time; }
+
+  Point3 at(double t) const
+  {
+    return ray_origin + t * ray_direction;
+  }
 
 private:
-    Point3 ray_origin;
-    Vector3 ray_direction;
+  Point3 ray_origin;
+  Vector3 ray_direction;
+  double ray_time;
 };
